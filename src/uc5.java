1e4e5bb039d6928903bcacf24696c51200cb4944
Srihari-Srivathsan-18 <ss4315@srmist.edu.in>
2026-02-17 11:21:15 +0530

    uc6

---
+++ src/uc5.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class uc5 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        // Push all characters into stack
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare by popping from stack
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("It is a Palindrome.");
+        } else {
+            System.out.println("It is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
